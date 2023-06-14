f9f0004d114d5495f00a52e8974eddf7318cbdec
Jaylyn Stample <jaystample@gmail.com>
2023-06-14 12:00:37 -0700

    Finalized! :+1:

--- src/app.js
+++ src/app.js
@@ -1,11 +1,14 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+let imageNumber = 0;
+document.getElementById("myButton").addEventListener("click", changeImage);
+let pic = [
+  "https://opengameart.org/sites/default/files/queen_of_diamonds2.png",
+  "https://opengameart.org/sites/default/files/king_of_hearts2.png",
+  "https://opengameart.org/sites/default/files/7_of_clubs.png",
+  "https://i.postimg.cc/SKdC8ygj/10-of-hearts.png",
+  "https://i.postimg.cc/DfHs1pPQ/ace-of-spades.png",
+  "https://i.postimg.cc/ZY2ssSSC/jack-of-hearts2.png",
+];
+function changeImage() {
+  imageNumber = (imageNumber + 1) % pic.length;
+  document.getElementById("getRanCard").src = pic[imageNumber];
+}
